328bb70395c5ac06f4f9e1acf0c61ac230c93935
Valentín Vera <valentinvera2805@gmail.com>
2024-10-07 16:17:37 -0300

    feat: Add @ to import, format code with prettier and initiliaze app of express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express"
 import cors from "cors"
 import { config } from "dotenv"
+import connectDB from "@/db/db"
 config()
 
-const PORT = 8080
-
 const app = express()
+const PORT = process.env.PORT ?? 8000
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+const initialize = async () => {
+  try {
+    await connectDB()
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
+    })
+  } catch (err) {
+    console.error("Error initializing: ", err)
+  }
+}
+
+initialize()
