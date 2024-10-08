435fc352ba497254270c87729cb000485178886d
Valentín Vera <valentinvera2805@gmail.com>
2024-10-08 17:48:54 -0300

    feat: Add controller for sign-up

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express"
+import User from "../models/user.model"
+import jwt from "jsonwebtoken"
+
+class AuthController {
+  async signUp(req: Request, res: Response): Promise<void> {
+    try {
+      const { user, email, password, phone, birthdate, balance } = req.body
+
+      const userExists = await User.findOne({ email })
+      if (userExists) {
+        res.status(400).json({ error: "Email already in use" })
+      }
+
+      const newUser = new User({
+        user,
+        email,
+        password: await new User().encryptPassword(password),
+        phone,
+        birthdate,
+        balance,
+      })
+
+      const savedUser = await newUser.save()
+      const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET || "secret", {
+        expiresIn: "1h",
+      })
+
+      res.status(201).json({ token })
+    } catch (error) {
+      console.error("Error in user registration: ", error)
+      const errorMessage = (error as Error).message || "Unknown error"
+      res.status(500).json({ message: "Registration failed", error: errorMessage })
+    }
+  }
+}
+
+export const authController = new AuthController()
