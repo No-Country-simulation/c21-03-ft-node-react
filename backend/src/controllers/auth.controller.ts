83ed70de908c714fdf8877cf914686bde1c39e27
Agus227 <aguscaro88@gmail.com>
2024-10-09 14:04:24 -0300

    Add signin logic for backend

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -34,6 +34,33 @@ class AuthController {
       res.status(500).json({ message: "Registration failed", error: errorMessage })
     }
   }
+  async signIn(req: Request, res: Response): Promise<void> {
+    try {
+      const { email, password } = req.body
+
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        res.status(400).json({ message: "Correo o contraseña inválidos" })
+      }
+
+      const isMatch = await bcrypt.compare(password, user!.password)
+
+      if (!isMatch) {
+        res.status(400).json({ message: "Correo o contraseña inválidos" })
+      }
+
+      const token = jwt.sign({ _id: user!._id }, process.env.JWT_SECRET || "secret", {
+        expiresIn: "1h",
+      })
+
+      res.status(200).json({ token })
+    } catch (error) {
+      console.error("Error logging in user: ", error)
+      const errorMessage = (error as Error).message || "Unknown error"
+      res.status(500).json({ message: "Sign in failed", error: errorMessage })
+    }
+  }
 }
 
 export const authController = new AuthController()
