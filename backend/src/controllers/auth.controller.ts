25c4c40cddcc770855b8ab96c896fbbd6b723738
Valentín Vera <valentinvera2805@gmail.com>
2024-10-09 17:21:36 -0300

    feat: Remove unnecessary import

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express"
 import User from "../models/user.model"
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
 
 class AuthController {
   async signUp(req: Request, res: Response): Promise<void> {
