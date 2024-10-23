e43cbddb720a310f0f3b4c6d419466187f7cfb95
Valentín Vera <valentinvera2805@gmail.com>
2024-10-23 17:34:56 -0300

    format: Code formatted with prettier

--- backend/src/utils/card.utils.ts
+++ backend/src/utils/card.utils.ts
@@ -1,12 +1,12 @@
 function generateCardNumber(): string {
-  const cardNumber = Array.from({ length: 16 }, () => Math.floor(Math.random() * 10)).join('')
-  return cardNumber.match(/.{1,4}/g)?.join(' ') || cardNumber
+  const cardNumber = Array.from({ length: 16 }, () => Math.floor(Math.random() * 10)).join("")
+  return cardNumber.match(/.{1,4}/g)?.join(" ") || cardNumber
 }
 
 function getExpiryDate(): string {
-  const currentDate = new Date();
+  const currentDate = new Date()
   const expiryYear = currentDate.getFullYear() + 5
-  const expiryMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0')
+  const expiryMonth = (currentDate.getMonth() + 1).toString().padStart(2, "0")
   return `${expiryMonth}/${expiryYear.toString().slice(-2)}`
 }
 
