64ffe2d3ee0e879a40ed65a4612fd5de8f914e92
Valentín Vera <valentinvera2805@gmail.com>
2024-10-23 00:19:05 -0300

    feat: Add config for the fonts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss"
+import defaultTheme from "tailwindcss/defaultTheme"
 
 const config: Config = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        "open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
+        "encode-sans": ["Encode Sans", ...defaultTheme.fontFamily.sans],
+      },
       boxShadow: {
         "1.5xl": "0px 4px 4px 0px #00000040",
       },
