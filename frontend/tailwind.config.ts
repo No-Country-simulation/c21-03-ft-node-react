bfdc8c431d1a13a8d802a77c88458faa80464090
Valentín Vera <valentinvera2805@gmail.com>
2024-10-22 23:50:03 -0300

    feat: Add custom box shadow for reuse

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "1.5xl": "0px 4px 4px 0px #00000040",
+      },
+    },
   },
   plugins: [],
 }
