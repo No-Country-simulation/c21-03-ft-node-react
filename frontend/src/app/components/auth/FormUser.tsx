6e5c53775ecfb8bf06d870fa3de050e2b32dea23
Valentín Vera <valentinvera2805@gmail.com>
2024-10-24 06:02:21 -0300

    feat: Add new styles and props

--- frontend/src/app/components/auth/FormUser.tsx
+++ frontend/src/app/components/auth/FormUser.tsx
@@ -2,7 +2,6 @@
 import { useState, ChangeEvent, FormEvent } from "react"
 import useAuth from "@/hooks/useAuth"
 import { useRouter } from "next/navigation"
-import "./FormUser.css"
 import Link from "next/link"
 import InputLabel from "./InputLabel"
 
@@ -15,14 +14,9 @@ const FormUser = ({ formType }: FormUserProps) => {
   const { signUp, signIn, loading, error } = useAuth()
   const [formData, setFormData] = useState({
     name: "",
-    surname: "",
-    username: "",
     email: "",
     password: "",
     confirmPassword: "",
-    birthdate: "",
-    phone: "",
-    balance: "",
   })
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -37,16 +31,11 @@ const FormUser = ({ formType }: FormUserProps) => {
     if (formType === "sign-up") {
       const registrationData = {
         user: {
-          username: formData.username,
           name: formData.name,
-          surname: formData.surname,
         },
         email: formData.email,
         password: formData.password,
         confirmPassword: formData.confirmPassword,
-        phone: formData.phone,
-        birthdate: formData.birthdate,
-        balance: parseFloat(formData.balance),
       }
 
       const response = await signUp(registrationData)
@@ -60,7 +49,6 @@ const FormUser = ({ formType }: FormUserProps) => {
 
     if (formType === "sign-in") {
       const loginData = {
-        email: formData.email,
         password: formData.password,
       }
 
@@ -75,113 +63,136 @@ const FormUser = ({ formType }: FormUserProps) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
-      {formType === "sign-up" && (
-        <>
-          <InputLabel
-            text="Nombre"
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Apellido"
-            type="text"
-            name="surname"
-            value={formData.surname}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Nombre de usuario"
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Email"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Contraseña"
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Confirmar contraseña"
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Fecha de Nacimiento"
-            type="date"
-            name="birthdate"
-            value={formData.birthdate}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Teléfono"
-            type="tel"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Balance"
-            type="tel"
-            name="balance"
-            value={formData.balance}
-            onChange={handleChange}
-          />
-        </>
-      )}
+    <div className="flex min-h-screen flex-col">
+      <form
+        onSubmit={handleSubmit}
+        className="form-container flex flex-1 flex-col items-center justify-center"
+      >
+        <fieldset>
+          {formType === "sign-in" && (
+            <div>
+              <legend className="mb-12 text-center font-open-sans text-4xl font-light text-[#4F4B4B]">
+                Bienvenido
+              </legend>
+              <div className="mb-10 flex gap-6">
+                <input
+                  type="text"
+                  className="customs-borders h-10 w-16 text-center text-2xl outline-none"
+                  maxLength={1}
+                  name="password"
+                  //   value={formData.password}
+                  onChange={handleChange}
+                  placeholder="1"
+                  required
+                />
+                <input
+                  type="text"
+                  className="customs-borders h-10 w-16 text-center text-2xl outline-none"
+                  maxLength={1}
+                  name="password"
+                  //   value={formData.password}
+                  onChange={handleChange}
+                  placeholder="2"
+                  required
+                />
+                <input
+                  type="text"
+                  className="customs-borders h-10 w-16 text-center text-2xl outline-none"
+                  maxLength={1}
+                  name="password"
+                  //   value={formData.password}
+                  onChange={handleChange}
+                  placeholder="3"
+                  required
+                />
+                <input
+                  type="text"
+                  className="customs-borders h-10 w-16 text-center text-2xl outline-none"
+                  maxLength={1}
+                  name="password"
+                  //   value={formData.password}
+                  onChange={handleChange}
+                  placeholder="4"
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+          {formType === "sign-up" && (
+            <>
+              <InputLabel
+                text="Nombre"
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="John Doe"
+              />
+              <InputLabel
+                text="Email"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="johndoe@gmail.com"
+              />
+              <InputLabel
+                text="Clave"
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="1234"
+              />
+              <InputLabel
+                text="Confirmar Clave"
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                placeholder="1234"
+              />
+            </>
+          )}
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="relative mx-auto mb-4 flex h-[60px] w-[240px] items-center justify-center rounded-[80px] bg-[#6630AC] text-center text-base font-bold text-white"
+          >
+            {loading ? "Cargando..." : formType === "sign-up" ? "Crear Cuenta" : "Iniciar Sesión"}
+          </button>
+
+          {error && <p className="text-red-500">{error}</p>}
+
+          {formType === "sign-in" && (
+            <div className="flex flex-col items-end gap-1.5">
+              <Link
+                href="/create-account"
+                className="font-roboto text-xs font-normal text-[#4F4B4B]"
+              >
+                Crear cuenta
+              </Link>
+              <Link href="/" className="font-roboto text-xs font-normal text-[#4F4B4B]">
+                Olvidaste tu contraseña
+              </Link>
+            </div>
+          )}
+        </fieldset>
+      </form>
 
       {formType === "sign-in" && (
-        <>
-          <p className="font-open-sans text-4xl font-light text-[#4F4B4B]">Bienvenido</p>
-          <input type="text" maxLength={1} className="" />
-          <InputLabel
-            text="Email"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          <InputLabel
-            text="Contraseña"
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </>
-      )}
-
-      <button type="submit" disabled={loading} className="form-button">
-        {loading ? "Cargando..." : formType === "sign-up" ? "Registrarse" : "Iniciar Sesión"}
-      </button>
-
-      {error && <p className="text-red-500">{error}</p>}
-
-      {formType === "sign-in" && (
-        <div>
-          <Link href="/create-account" className="font-roboto text-xs font-normal text-[#4F4B4B]">
-            Crear cuenta
+        <div className="mt-auto flex w-full items-center justify-between px-16 py-6">
+          <Link href="/create-account" className="font-roboto text-xs font-normal text-[#6630AC]">
+            EMPRESA
           </Link>
-          <Link href="/" className="font-roboto text-xs font-normal text-[#4F4B4B]">
-            Olvidaste tu contraseña
+          <Link href="/" className="font-roboto text-xs font-normal text-[#6630AC]">
+            ADMINISTRADOR
           </Link>
         </div>
       )}
-    </form>
+    </div>
   )
 }
 
