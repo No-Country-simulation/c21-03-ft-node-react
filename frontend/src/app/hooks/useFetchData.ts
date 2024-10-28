abedfddaaa8b976f2e9b3adf8976a3a652c00aaf
Valentín Vera <valentinvera2805@gmail.com>
2024-10-28 20:46:36 -0300

    feat: Add hook for fetching bank transfer data and rendering them

---
+++ frontend/src/app/hooks/useFetchData.ts
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react"
+
+interface User {
+  name: string
+  bankName: string
+  CBU: string
+  _id: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export const useFetchData = (name: string) => {
+  const [data, setData] = useState<User | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (name.trim() === "") {
+      setData(null)
+      setLoading(false)
+      return
+    }
+
+    const fetchData = async () => {
+      setLoading(true)
+
+      try {
+        const response = await fetch(`http://localhost:8000/api/transfer/get-transfer/${name}`)
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+
+        const data = await response.json()
+
+        const userData: User = {
+          name: data.account.name,
+          bankName: data.account.bankName,
+          CBU: data.account.CBU,
+          _id: data.account._id,
+          createdAt: data.account.createdAt,
+          updatedAt: data.account.updatedAt,
+        }
+
+        setData(userData)
+      } catch (err) {
+        console.error(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [name])
+
+  return [data, loading] as const
+}
