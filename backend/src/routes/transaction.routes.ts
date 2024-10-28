ab04c2ea798b72a525e053b7d3c2cb8fd0c939c4
Valentín Vera <valentinvera2805@gmail.com>
2024-10-28 20:52:13 -0300

    refactor: Remove unnecessary files

--- backend/src/routes/transaction.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { transactionController } from "../controllers/transaction.controller"
-
-const transactionRouter = Router()
-
-transactionRouter.post("/create", transactionController.post)
-transactionRouter.put("/addMoney", transactionController.addMoney)
-
-export default transactionRouter
