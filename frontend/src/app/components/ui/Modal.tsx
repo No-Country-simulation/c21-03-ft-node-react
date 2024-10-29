222099ffc9645abb121108c81ae8f02502196b03
Valentín Vera <valentinvera2805@gmail.com>
2024-10-29 01:55:54 -0300

    feat: Add amount in the interface and within a paragraph to render it

--- frontend/src/app/components/ui/Modal.tsx
+++ frontend/src/app/components/ui/Modal.tsx
@@ -5,6 +5,7 @@ export interface User {
   name: string
   bankName: string
   CBU: string
+  amount: number
   _id: string
   createdAt?: Date
   updatedAt?: Date
@@ -17,7 +18,7 @@ interface ModalProps {
 const Modal = ({ user }: ModalProps) => {
   return (
     <Link
-      className="relative mx-auto mb-40 flex h-[110px] w-[271px] flex-col rounded-[25px] bg-[#F3EDF7] pl-4 pt-4 font-encode-sans shadow-md"
+      className="relative mx-auto mb-40 flex h-[120px] w-[271px] flex-col rounded-[25px] bg-[#F3EDF7] pl-4 pt-4 font-encode-sans shadow-md"
       href={`/operations/transfers/transfer-to/scheduled-account/${encodeURIComponent(user.name)}`}
       key={user._id}
     >
@@ -32,6 +33,7 @@ const Modal = ({ user }: ModalProps) => {
         <h2>{user.name}</h2>
         <p className="text-pale-blue text-[14px]">{user.bankName}</p>
         <span className="text-[14px]">{user.CBU}</span>
+        <span className="text-[14px]">{user.amount.toLocaleString("es-AR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
       </div>
     </Link>
   )
